644f0ceefb099cc78d56faa2d10f15e73154724f
sanjeevsenapati <sanjeev@therasoftonline.com>
2026-09-12 11:37:26 +0530

    Deploy static site with base and .nojekyll

---
+++ assets/chunks/swimlanesDiagram-VR7AAH4N.CiuVy07O.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.ChLplQ3O.js";import{_ as a}from"../app.BCPufSVO.js";import"./chunk-5VM5RSS4.DNok9uSS.js";import"./chunk-XXDRQBXY.BCH2QprM.js";import"./chunk-POPQ4Y6H.C00rP6gr.js";import"./chunk-F27PBJKO.BdZ2RecW.js";import"./channel.xQ1wWobs.js";import"./framework.Dxah4DFW.js";import"./theme.CGu5ZSFx.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
