04877f66193033d5b363050e8f70382996b48fc7
sanjeevsenapati <sanjeev@therasoftonline.com>
2026-09-12 11:35:27 +0530

    Deploy static site

---
+++ assets/chunks/swimlanesDiagram-VR7AAH4N.CIgqA_SC.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.B4fWqbKu.js";import{_ as a}from"../app.Cjo8jqBF.js";import"./chunk-5VM5RSS4.CoMAZrBR.js";import"./chunk-XXDRQBXY.DN7J1Sek.js";import"./chunk-POPQ4Y6H._D3bQSl6.js";import"./chunk-F27PBJKO.DvY6TEVO.js";import"./channel.D2WpzU7H.js";import"./framework.vp7jW9z7.js";import"./theme.hVVK4iV2.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
